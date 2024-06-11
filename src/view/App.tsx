0ed3a02b71c5516632a046b94cb1b38ee0b9df11
Tal Yaron <tal.yaron@gmail.com>
2024-06-11 17:09:15 +0300

    react query ready

--- src/view/App.tsx
+++ src/view/App.tsx
@@ -1,23 +1,30 @@
-import { useState, useEffect } from "react";
+
 import "./styles/globals.scss";
 import { getStatements } from "../controllers/db/statements/getStatements";
 import { Statement } from "delib-npm";
+import { useQuery } from 'react-query';
+
 
 function App() {
-  const [statements, setStatements] = useState<Statement[]>([]);
+ 
 
-  useEffect(() => {
-    (async () => {
-      const _statements = await getStatements();
+  const { isLoading, error, data: statements = [] } = useQuery(
+    ['statements'],
+    async () => {
+      const _statements:Statement[] = await getStatements();
+      return _statements;
+    }
+  );
 
-      setStatements(_statements);
-    })();
-  }, []);
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error: An error occurred.</div>;
+
   return (
+    <>
     <div>
       <h1>Statements</h1>
       <section>
-        {statements.map((statement) => (
+        {statements.map((statement:Statement) => (
           <div key={statement.statementId}>
             <h2>{statement.statement}</h2>
             <hr />
@@ -25,6 +32,7 @@ function App() {
         ))}
       </section>
     </div>
+    </>
   );
 }
 
