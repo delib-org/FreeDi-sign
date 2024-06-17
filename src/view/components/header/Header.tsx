e9688836831ebc1fd90dfd0acfb807919b13c542
Tal Yaron <tal.yaron@gmail.com>
2024-06-17 09:06:38 +0300

    header WIP

--- src/view/components/header/Header.tsx
+++ src/view/components/header/Header.tsx
@@ -1,12 +1,36 @@
 import styles from "./header.module.scss";
+
+//icons
 import Logo from "../../../assets/logo/monoLogo.svg?react";
+import Home from "../../../assets/icons/home.svg?react";
+import Search from "../../../assets/icons/search.svg?react";
+import Install from "../../../assets/icons/install.svg?react";
 
 const Header = () => {
   return (
     <header className={styles.header}>
       <div className={styles.header__company}>
-        <Logo />
-        <div className="logo-font">FreeDi</div>
+        <div className={styles.header__company__logo}>
+          <Logo />
+          <div className="logo-text">FreeDi</div>
+        </div>
+        <div className={styles.header__company__testimonials}>
+          For Nordia Community
+        </div>
+      </div>
+      <div className={styles.header__right}>
+        <div className={styles.header__right__icons}>
+          <button className={styles.header__right__icons__icon}>
+            <Home />
+          </button>
+          <button className={styles.header__right__icons__icon}>
+           <Search />
+          </button>
+          <button className={styles.header__right__icons__icon}>
+            <Install />
+          </button>
+        </div>
+        <div className={styles.header__right__lastModified}>Last Modified: 09:05 17/6/2024</div>
       </div>
     </header>
   );
