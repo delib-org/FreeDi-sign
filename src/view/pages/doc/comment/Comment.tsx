76474a99727749ae3c660a6d4fa3c90c170315af
Tal Yaron <tal.yaron@gmail.com>
2024-07-11 09:21:00 +0300

    we have approval buttons

--- src/view/pages/doc/comment/Comment.tsx
+++ src/view/pages/doc/comment/Comment.tsx
@@ -1,14 +1,20 @@
-import { Statement } from 'delib-npm'
-import {FC} from 'react'
-interface Props{
-    statement:Statement;
-}
-const Comment:FC<Props> = ({statement}) => {
-  return (
-    <div className='comment'>
-        comment: {statement.statement}
-    </div>
-  )
-}
+import { Statement } from "delib-npm";
+import { FC } from "react";
+import styles from "./Comment.module.scss";
 
-export default Comment
+interface Props {
+  statement: Statement;
+}
+const Comment: FC<Props> = ({ statement }) => {
+  return (
+    <div className={styles.comment}>
+      <div className={styles.comment__approval}>
+        <button>Approve</button>
+        <button>Reject</button>
+      </div>
+      <p>comment: {statement.statement}</p>
+    </div>
+  );
+};
+
+export default Comment;
