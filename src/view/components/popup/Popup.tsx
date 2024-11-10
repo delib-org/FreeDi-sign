d5f4a167c1a4e8219236c666bafcfab2b642edca
Tal Yaron <tal.yaron@gmail.com>
2024-11-10 10:41:43 +0200

    feedback for siging is ready

--- src/view/components/popup/Popup.tsx
+++ src/view/components/popup/Popup.tsx
@@ -1,13 +1,21 @@
 import { FC } from "react";
 import styles from "./Popup.module.scss";
-import { mySignatureUpdateSelector, setMySignatureUpdate } from "../../../controllers/slices/statementsSlice";
+import {
+  mySignatureUpdateSelector,
+  setMySignatureUpdate,
+} from "../../../controllers/slices/statementsSlice";
 import { useDispatch, useSelector } from "react-redux";
+
+//icons
+import CloseIcon from "../../../assets/icons/close.svg?react";
+import { useLanguage } from "../../../controllers/hooks/useLanguage";
 
 interface Props {
   statementId: string;
 }
 const Popup: FC<Props> = ({ statementId }) => {
   const dispatch = useDispatch();
+  const {t} = useLanguage();
   const signatureUpdate = useSelector(mySignatureUpdateSelector(statementId));
   const showPopup = signatureUpdate && signatureUpdate.signed !== undefined;
   if (!showPopup) return null;
@@ -16,18 +24,22 @@ const Popup: FC<Props> = ({ statementId }) => {
     dispatch(setMySignatureUpdate({ statementId, signed: undefined }));
   }
 
-  const popupBackground = signatureUpdate.signed ? styles["popup--approve"] : styles["popup--reject"];
+  const popupBackground = signatureUpdate.signed
+    ? styles["popup--approve"]
+    : styles["popup--reject"];
+
+  const word = signatureUpdate.signed ? "signature" : "rejection";
 
   return (
-    <div className={`${styles.popup} ${popupBackground}`}>
-      <div>
-        <div className={styles["popup-content"]}>
-          <button className="close" onClick={handleHidePopup}>
-            &times;
-          </button>
-          <p>This is a pop-up message!</p>
-        </div>
+    <div className={`${styles.popup} ${popupBackground}`} onClick={handleHidePopup}>
+     
+      <div className={styles["popup-content"]}>
+        <p>{t("Thank you")}!</p>
+        <p>{t(`Your ${word} has been sent`)}</p>
       </div>
+      <button className={styles.close}>
+        <CloseIcon />
+      </button>
     </div>
   );
 };
