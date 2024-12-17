8b773ab99985b2da64f1c84eae1565dc9e3aebf7
Tal Yaron <tal.yaron@gmail.com>
2024-12-17 09:34:46 +0200

    add options on menu

--- src/view/pages/doc/aside/Aside.tsx
+++ src/view/pages/doc/aside/Aside.tsx
@@ -2,28 +2,24 @@ import LogoAndNameIcon from "../../../components/icons/LogoAndNameIcon";
 import styles from "./Aside.module.scss";
 
 import {
-  LanguagesEnum,
   useLanguage,
 } from "../../../../controllers/hooks/useLanguage";
 
 //icons
-import USAFlag from "../../../../assets/icons/usaFlag.svg?react";
-import IsraelFlag from "../../../../assets/icons/israelFlag.svg?react";
+// import USAFlag from "../../../../assets/icons/usaFlag.svg?react";
+// import IsraelFlag from "../../../../assets/icons/israelFlag.svg?react";
 import TOC from "../toc/TOC";
-import CSVDownloadButton from "../../../components/buttons/downloadCSV/DownloadCSV";
-import { Role } from "delib-npm";
 
-interface Props{
-  role: Role;
-}
 
-function Aside({role}: Props) {
-  const { t, dir, currentLanguage, changeLanguage } = useLanguage();
+import BottomAside from "./bottomAside/BottomAside";
 
-  function handleToggleLanguage() {
-    if (currentLanguage === LanguagesEnum.he) changeLanguage(LanguagesEnum.en);
-    else changeLanguage(LanguagesEnum.he);
-  }
+function Aside() {
+  const { t, dir } = useLanguage();
+
+  // function handleToggleLanguage() {
+  //   if (currentLanguage === LanguagesEnum.he) changeLanguage(LanguagesEnum.en);
+  //   else changeLanguage(LanguagesEnum.he);
+  // }
 
   return (
     <aside className={styles.aside}>
@@ -36,15 +32,17 @@ function Aside({role}: Props) {
         <div className={styles.slogan}>{t("Fostering Collaboration")}</div>
       </div>
       <TOC isAside={true} />
+      
+     
+      <BottomAside />
       <div className={styles.ddi}>
-        <button className={styles.button} onClick={handleToggleLanguage}>
+        {/* <button className={styles.button} onClick={handleToggleLanguage}>
           {currentLanguage === LanguagesEnum.en ? <IsraelFlag /> : <USAFlag />}
-        </button>
+        </button> */}
         <a href="https://delib.org" target="_blank">
           {t("From the Deliberative Democracy Institute")}
         </a>
       </div>
-      {role === Role.admin && <CSVDownloadButton />}
     </aside>
   );
 }
---
+++ src/view/pages/doc/aside/bottomAside/BottomAside.tsx
@@ -0,0 +1,38 @@
+import React, { useContext } from "react";
+import styles from "./BottomAside.module.scss";
+import { useParams } from "react-router-dom";
+import { CSV } from "../../../../../controllers/general.ts/csv";
+
+//icons
+import DownloadIcon from "../../../../../assets/icons/downloadCloud.svg?react";
+import GlobousIcon from "../../../../../assets/icons/globus.svg?react";
+import { DocumentContext } from "../../documentCont";
+import { Role } from "delib-npm";
+
+const BottomAside: React.FC = () => {
+  const { statementId: documentId } = useParams<{ statementId: string }>();
+  const {role} = useContext(DocumentContext);
+
+  function onDownloadClick() {
+    console.log("download clicked");
+    CSV.downloadCSV(documentId);
+  }
+  return (
+    <div className={styles.options}>
+      {role === Role.admin && <div className={styles.clickable} onClick={onDownloadClick}>
+        <DownloadIcon />
+        <span>Download CSV</span>
+      </div>}
+      <a
+        className={styles.websiteSection}
+        href="https://freedi.co"
+        target="_blank"
+      >
+        <GlobousIcon />
+        <span>FreeDi</span>
+      </a>
+    </div>
+  );
+};
+
+export default BottomAside;
